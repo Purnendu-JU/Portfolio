06d112da782246cec7a367d000304743e7d9074d
Purnendu-JU <163437589+Purnendu-JU@users.noreply.github.com>
2024-07-17 12:07:34 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,10 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+
+let btn = document.getElementById("submit");
+btn.addEventListener('click', showMsg);
+function showMsg(){
+    alert("You will be contacted soon !!!");
+}
